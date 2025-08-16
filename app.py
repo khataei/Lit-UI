b41c2af945961aedf2f31214b7cacf06f2965542
khataei <skhataeipour@mun.ca>
2025-08-16 00:11:02 -0700

    tried fox logo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import base64
 import html
 
 
-page_icon = '.\assets\fox-icon.png' #'🤖' 
+page_icon = 'assets/fox-icon.png' #'🤖' 
 page_layout = 'wide'
 page_title = 'CarfaxGPT'
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=page_layout)
